28d54277382ceb7fae21812ea1f085d240cbf24b
Ashley Santana <ashleysantana053@gmail.com>
2023-11-13 10:14:21 -0300

    Criaçaõ do arquivo soma, que realiza a soma entre dois valores

---
+++ primeiro.c
@@ -0,0 +1,10 @@
+//utilização de variáveis
+//incluir a biblioteca stdio para entrada e saídad e dados
+#include <stdio.h>
+int main(){
+    //declaração da varaiável valor com o tipo inteiro e número 10
+    int valor =24;
+    printf("Meu irmão tem %d anos de idade\n",valor);
+    
+    return 0;
+}
