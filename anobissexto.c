b13e7a9ee52049423487a434b79c832bce2324b4
Ashley Santana <ashleysantana053@gmail.com>
2023-11-14 10:08:46 -0300

    Criação do arquivo para calcular o anobissexto

---
+++ anobissexto.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h> //comando system para limpar a tela
+
+int main(){
+
+    //vamos usar o comando system(/bin/clear) (clear screen) para
+    //limpar a tela antes de executar os demais comandos
+    system("bin/clear");
+    //system("clear");
+    int num;
+    printf("Difite o ano e tecle enter\n");
+    scanf("%d",&num);
+    if(num % 4 == 0)
+        printf("O ano %d é anobissexto\n",num);
+    else
+        printf("O ano não é anobissexto\n",num);
+
+return 0;
+}
---
+++ desvio1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int num;
+    printf("Digite um número:\n");
+    scanf("%d",&num);
+    if(num > 10)
+        printf("O número é maior que 10\n");
+    else
+    printf("O número digitado é menor que 10\n");
+return 0;
+
+}
